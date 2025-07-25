e59b0fec775f08ce246a675ddc7e4cf748b63a50
GACoutts <ST10258297@imconnect.edu.za>
2025-07-25 14:36:50 +0200

    File read started

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
     }
 
-    public static void getFile(){
-        
+    public static void getFile(String fileName){
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while((line = reader.readLine()) != null){
+                
+            }
+        }catch(IOException e){
+            System.out.println("Error: " + e.getLocalizedMessage());
+        }
     }
 }
