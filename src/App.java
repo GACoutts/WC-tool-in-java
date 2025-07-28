0d968ca2657607716a04daad4d98220d773945d7
GACoutts <ST10258297@imconnect.edu.za>
2025-07-28 15:21:44 +0200

    File read done

--- src/App.java
+++ src/App.java
@@ -2,20 +2,34 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class App {
+    static String line = "";
     public static void main(String[] args) throws Exception {
-        
+        String cmd = System.in.toString();
+        switch(cmd){
+         case "-c":
+
+            break;
+        }
     }
 
-    public static void getFile(String fileName){
+    public static String getFile(String fileName){
+        
         try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
-            String line;
             while((line = reader.readLine()) != null){
                 
             }
         }catch(IOException e){
             System.out.println("Error: " + e.getLocalizedMessage());
         }
+        return line;
+    }
+
+    public static void numOfBytes(){
+        RandomAccessFile raf = new RandomAccessFile("/Users/grantcoutts/Documents/Programing_projects/text.txt");
+        fl = raf.length();
+        byte[] nob = new byte[]
     }
 }
